c0590642615988dec31a707cf50526817cd5e260
Mo Yin <yin>
2019-09-03 16:46:10 +0100

    changed theme to shiny app

--- runsimulationcode_257019.R
+++ runsimulationcode_257019.R
@@ -1,6 +1,6 @@
 rm(list=ls())
-setwd("/Users/moyin/Desktop/NItrialsimulation-master/") #set working directory 
-source(file = 'causalinference 11 March 2019 (no AT and matching).R')
+setwd("/Users/moyin/Documents/nBox/git_projects/NItrialsimulation/") #set working directory 
+source(file = 'simulationcode_110319.R')
 
 ######parameters######
 #assuming that the experimental and control treatments have the same treatment efficacy of 60%
@@ -16,10 +16,12 @@ true.effect=p.experiment-p.stdcare
 nIterations=1000
 NImargin=0.1
 
-width=10
-height= 15
+width=15
+height= 10
 
-#Run simulations for case 1 (both groups non compliant)
+legendfontsize=12
+
+#Run simulations for case 1 (both groups non compliant) 
 b1both<-bias.nonconfounding(n=n, p.experiment=p.experiment, p.stdcare=p.stdcare,  nIterations=nIterations, interval = interval, noncomply = "both",true.effect=true.effect)
 t1both<-type1.nonconfounding(n=n, p.experiment=p.experiment, NImargin=NImargin,  nIterations=nIterations, interval = interval,noncomply = "both")
 case1<-ggarrange(b1both, t1both,  ncol = 2, labels = c('A','B'), 
@@ -34,18 +36,19 @@ case2<-ggarrange(b2both1, t2both1, ncol = 2, labels = c('A','B'),
                     common.legend = TRUE, legend = "bottom" )
 ggsave(filename = paste("case2", Sys.Date(), ".pdf"), width = width, height = height, units = "in")
 
-## higher value of confounder makes intervention more likely, outcome more likely
+## Run simulations for case 3: higher value of confounder makes intervention more likely, outcome more likely
 b2both2<-bias.confounding (n=n, p.experiment=p.experiment, p.stdcare=p.stdcare, interval = interval,confounder.intervention="Increase likelihood", confounder.outcome="Increase likelihood",nIterations=nIterations, noncomply = "both", true.effect=true.effect)
 t2both2<-type1.confounding(n=n, p.experiment=p.experiment, NImargin=NImargin,  confounder.intervention="Increase likelihood", confounder.outcome="Increase likelihood", nIterations=nIterations, interval = interval, noncomply = "both")
 case3<-ggarrange(b2both2, t2both2, ncol = 2, labels = c('A','B'), 
                  common.legend = TRUE, legend = "bottom" )
 ggsave(filename = paste("case3", Sys.Date(), ".pdf"), width = width, height = height, units = "in")
 
-#Run simulations for case 3: Non compliance with unknwon confounders 
-bknown<-bias.unknownconfounding (n=n, p.experiment=p.experiment, p.stdcare=p.stdcare, interval = interval,confounder.intervention="Increase likelihood", confounder.outcome="Decrease likelihood",nIterations=nIterations, noncomply = "both", true.effect=true.effect)
-tknown<-type1.unknownconfounding(n=n, p.experiment=p.experiment, NImargin=NImargin,  confounder.intervention="Increase likelihood", confounder.outcome="Decrease likelihood", nIterations=nIterations, interval = interval, noncomply = "both")
-case4<-ggarrange(bknown, tknown, ncol = 2, labels = c('A','B'), 
-                 common.legend = TRUE, legend = "bottom" )
-ggsave(filename = paste("case4", Sys.Date(), ".pdf"), width = width, height = height, units = "in")
+#Run simulations for case 4: Non compliance with unknown confounders 
+#bknown<-bias.unknownconfounding (n=n, p.experiment=p.experiment, p.stdcare=p.stdcare, interval = interval,confounder.intervention="Increase likelihood", confounder.outcome="Decrease likelihood",nIterations=nIterations, noncomply = "both", true.effect=true.effect)
+#tknown<-type1.unknownconfounding(n=n, p.experiment=p.experiment, NImargin=NImargin,  confounder.intervention="Increase likelihood", confounder.outcome="Decrease likelihood", nIterations=nIterations, interval = interval, noncomply = "both")
+#case4<-ggarrange(bknown, tknown, ncol = 2, labels = c('A','B'), 
+#                 common.legend = TRUE, legend = "bottom" )
+#ggsave(filename = paste("case4", Sys.Date(), ".pdf"), width = width, height = height, units = "in")
 
 bmulti<-bias.unknownconfounding.multi(n=n, p.experiment=p.experiment, p.stdcare=p.stdcare, interval = interval,confounder.intervention="Decrease likelihood", confounder.outcome="Increase likelihood",nIterations=nIterations, noncomply = "both")
+ggsave(filename = paste("unknownmulti", Sys.Date(), ".pdf"), width = width, height = height*2/3, units = "in")
